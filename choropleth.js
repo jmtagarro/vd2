0472feb2c7bc657af2d4def975ad74231c2c401f
Jose Tagarro <chema92@gmail.com>
2024-04-20 18:56:37 +0200

    Choropleth basico

--- choropleth.js
+++ choropleth.js
@@ -7,30 +7,27 @@ const margin2 = {top: 10, right: 10, bottom: 10, left: 10},
 const svg2 = d3.select("#choropleth")
   .append("svg")
     .attr("width", width2 + margin2.left + margin2.right)
-    .attr("height", height2 + margin2.top + margin2.bottom)
-  .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
+    .attr("height", height2 + margin2.top + margin2.bottom);
+
 
 // Map and projection
 const path = d3.geoPath();
 const projection = d3.geoMercator()
-  .scale(100)
-  .center([0,20])
-  .translate([width2 / 2, height2 / 2]);
+  .scale(6000)
+  .center([-5,37.5])
+  .translate([width / 2, height / 2]);
 
 // Data and color scale
 let data = new Map()
-const colorScale = d3.scaleThreshold()
-  .domain([10, 100, 1000, 3000, 10000, 50000])
-  .range(d3.schemeBlues[7]);
+const colorScale = d3.scaleQuantize([0, 50], d3.schemeBlues[9]);
 
 // Load external data and bootcod_disbar
 Promise.all([
-d3.json("distritos.geojson"),
-d3.csv("tiques_20240304.csv")]).then(function(loadData){
+d3.json("municipios.geojson"),
+d3.dsv(";", "da_centros.csv")]).then(function(loadData){
   let topo = loadData[0]
-  let tiques = loadData[1]
-  let count = d3.rollup(tiques, v => v.length, d => d.cod_distrito)
+  let centros = loadData[1]
+  let count = d3.rollup(centros, v => v.length, d => d.cod_municipio)
 
   svg2.append("g")
     .selectAll("path")
@@ -40,7 +37,7 @@ d3.csv("tiques_20240304.csv")]).then(function(loadData){
       .projection(projection)
     )
     .attr("fill", function (d) {
-      d.total = count.get(d.COD_DIS_TX) || 0;
+      d.total = count.get(d.properties.NATCODE.slice(-5)) || 5;
       return colorScale(d.total);
     })
 })

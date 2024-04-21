d557a678c8f9b48973c3183c00681ec068cb870c
Jose Tagarro <chema92@gmail.com>
2024-04-21 22:43:39 +0200

    First working marimekko

--- marimekko.js
+++ marimekko.js
@@ -1,100 +1,80 @@
-// set the dimensions and margins of the graph
-const width3 = 860,
-    height3 = 500,
-    margin3 = 20;
+// Specify the chart’s dimensions.
+const width3 = 860;
+const height3 = 400;
+const marginTop = 30;
+const marginRight = -1;
+const marginBottom = -1;
+const marginLeft = 1;
 
-const x = d3.scaleLinear()
-.range([0, width3 - 3 * margin3]);
+// Create the SVG container.
 
-const y = d3.scaleLinear()
-    .range([0, height3 - 2 * margin3]);
-
-const z = d3.scaleOrdinal(d3.schemeCategory10)
-
-const n = d3.format(",d"),
-    p = d3.format("%");
-
-// append the svg object to the body of the page
 const svg3 = d3.select("#marimekko")
   .append("svg")
+    .attr("viewBox", [0, 0, width3, height3])
     .attr("width", width3)
     .attr("height", height3)
-  .append("g")
-    .attr("transform", "tdataranslate(" + 2 * margin3 + "," + margin3 + ")");
+    .attr("style", "max-width: 100%; height: auto; font: 10px sans-serif;");
 
-d3.json("marimekko.json", function(error, data) {
-  if (error) throw error;
+d3.json("marimekko.json").then( function(data) {
 
-  let offset = 0;
+// Create the color scale.
+const color = d3.scaleOrdinal(d3.schemeCategory10).domain(data.map(d => d.Tipo));
 
-  // Nest values by segment. We assume each segment+market is unique.
-  let tipos = d3.nest()
-      .key(function(d) { return d.Tipo; })
-      .entries(data);
+// Compute the layout.
+const treemap = data => d3.treemap()
+    .round(true)
+    .tile(d3.treemapSliceDice)
+    .size([
+        width3 - marginLeft - marginRight, 
+        height3 - marginTop - marginBottom
+    ])
+    (d3.hierarchy(d3.group(data, d => d.Provincia, d => d.Tipo)).sum(d => d.Consumo))
+    .each(d => {
+    d.x0 += marginLeft;
+    d.x1 += marginLeft;
+    d.y0 += marginTop;
+    d.y1 += marginTop;
+    });
 
-  console.log(tipos)
+const root = treemap(data);
 
-  // Compute the total sum, the per-segment sum, and the per-market offset.
-  // You can use reduce rather than reduceRight to reverse the ordering.
-  // We also record a reference to the parent segment for each market.
-  const sum = tipos.reduce(function(v, p) {
-    return (p.offset = v) + (p.sum = p.values.reduceRight(function(v, d) {
-      d.parent = p;
-      return (d.offset = v) + d.Consumo;
-    }, 0));
-  }, 0);
+// Position the nodes.
+const node = svg3.selectAll("g")
+    .data(root.descendants())
+    .join("g")
+    .attr("transform", d => `translate(${d.x0},${d.y0})`);
 
-  // Add x-axis ticks.
-  const xtick = svg3.selectAll(".x")
-      .data(x.ticks(10))
-    .enter().append("g")
-      .attr("class", "x")
-      .attr("transform", function(d) { return "translate(" + x(d) + "," + y(1) + ")"; });
+const format = d => d.toLocaleString();
 
-  xtick.append("line")
-      .attr("y2", 6)
-      .style("stroke", "#000");
+// Draw column labels.
+const column = node.filter(d => d.depth === 1);
 
-  xtick.append("text")
-      .attr("y", 8)
-      .attr("text-anchor", "middle")
-      .attr("dy", ".71em")
-      .text(p);
+column.append("text")
+    .attr("x", 3)
+    .attr("y", "-1.7em")
+    .style("font-weight", "bold")
+    .text(d => d.data[0]);
 
-  // Add y-axis ticks.
-  const ytick = svg3.selectAll(".y")
-      .data(y.ticks(10))
-    .enter().append("g")
-      .attr("class", "y")
-      .attr("transform", function(d) { return "translate(0," + y(1 - d) + ")"; });
+column.append("text")
+    .attr("x", 3)
+    .attr("y", "-0.5em")
+    .attr("fill-opacity", 0.7)
+    .text(d => format(d.value));
 
-  ytick.append("line")
-      .attr("x1", -6)
-      .style("stroke", "#000");
+column.append("line")
+    .attr("x1", -0.5)
+    .attr("x2", -0.5)
+    .attr("y1", -30)
+    .attr("y2", d => d.y1 - d.y0)
+    .attr("stroke", "#000")
 
-  ytick.append("text")
-      .attr("x", -8)
-      .attr("text-anchor", "end")
-      .attr("dy", ".35em")
-      .text(p);
+// Draw leaves.
+const cell = node.filter(d => d.depth === 2);
 
-  // Add a group for each segment.
-  let segments = svg3.selectAll(".tipo")
-      .data(tipos)
-    .enter().append("g")
-      .attr("class", "tipo")
-      .attr("xlink:title", function(d) { return d.key; })
-      .attr("transform", function(d) { return "translate(" + x(d.offset / sum) + ")"; });
+cell.append("rect")
+    .attr("fill", d => color(d.data[0]))
+    .attr("fill-opacity", (d, i) => d.Consumo / d.parent.Consumo)
+    .attr("width", d => d.x1 - d.x0 - 1)
+    .attr("height", d => d.y1 - d.y0 - 1);
 
-  // Add a rect for each market.
-  const provincias = tipos.selectAll(".provincia")
-      .data(function(d) { return d.values; })
-    .enter().append("a")
-      .attr("class", "provincia")
-      .attr("xlink:title", function(d) { return d.Provincias + " " + d.parent.key + ": " + n(d.Consumo); })
-    .append("rect")
-      .attr("y", function(d) { return y(d.offset / d.parent.sum); })
-      .attr("height", function(d) { return y(d.value / d.parent.sum); })
-      .attr("width", function(d) { return x(d.parent.sum / sum); })
-      .style("fill", function(d) { return z(d.Provincias); });
-});
+})
